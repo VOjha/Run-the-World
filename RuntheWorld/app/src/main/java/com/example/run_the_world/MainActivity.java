f6743d59bc7b863c27c3fe6caf780c1a5a873d04
Vidushi Ojha <vidushi.ojha@gmail.com>
2019-11-16 15:55:21 -0600

    Homepage and entering run data page

--- RuntheWorld/app/src/main/java/com/example/run_the_world/MainActivity.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.run_the_world;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button enterRunButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterRunButton = (Button) findViewById(R.id.enter_run);
+
+        enterRunButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent enterRunIntent = new Intent(this, InputRunData.class);
+        enterRunIntent.setAction(Intent.ACTION_VIEW);
+
+        startActivity(enterRunIntent);
     }
 }
