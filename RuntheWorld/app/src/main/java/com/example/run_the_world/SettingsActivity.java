91b9d3e005b4c92e266c182fe2b0a8f8144605b3
swaathi2 <swaathi2@illinois.edu>
2019-11-16 17:21:42 -0600

    settings_switch

--- RuntheWorld/app/src/main/java/com/example/run_the_world/SettingsActivity.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.run_the_world;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Spinner spinner = (Spinner) findViewById(R.id.dist_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.dist_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
+
+
 }
