84d78eb27e8ee28ea3c0de783d6a383ea95fecc9
Dingyu Peng <dpeng4@illinois.edu>
2019-11-17 17:23:43 -0600

    data transfer
    created summary activity
    during run -> summary -> distance_log
    input_run_data -> summary -> distance_log

--- RuntheWorld/app/src/main/java/com/example/run_the_world/DuringRun.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/DuringRun.java
@@ -2,7 +2,11 @@ package com.example.run_the_world;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DuringRun extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class DuringRun extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_during_run);
+
+        Button btn = findViewById(R.id.done);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText input_distance = (EditText) findViewById(R.id.input_distance);
+                String distance = input_distance.getText().toString();
+
+                final EditText input_time = (EditText) findViewById(R.id.input_time);
+                String time = input_time.getText().toString();
+
+                Intent intent = new Intent(DuringRun.this, summaryActivity.class);
+                intent.putExtra("distance", distance);
+                intent.putExtra("time", time);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/summaryActivity.java
@@ -0,0 +1,38 @@
+package com.example.run_the_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class summaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+
+        Intent intent = getIntent();
+
+        String time = intent.getStringExtra("time");
+        TextView timeView = (TextView)findViewById(R.id.time);
+        timeView.setText("Total run time: " + time + " min");
+
+        String distance = intent.getStringExtra("distance");
+        TextView distanceView = (TextView)findViewById(R.id.distance);
+        distanceView.setText("Total run distance: " + distance + " km");
+
+        Button btn = findViewById(R.id.Continue);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(summaryActivity.this, distance_log.class));
+            }
+        });
+    }
+
+}
