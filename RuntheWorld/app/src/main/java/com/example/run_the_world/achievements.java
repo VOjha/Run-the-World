7653720d218e313093aba609a3e79ada03d799a9
gdandu2 <57690677+gdandu2@users.noreply.github.com>
2019-11-18 16:55:16 -0600

    added connections to distance log and travel book

--- RuntheWorld/app/src/main/java/com/example/run_the_world/achievements.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/achievements.java
@@ -2,13 +2,38 @@ package com.example.run_the_world;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class achievements extends AppCompatActivity {
+public class achievements extends AppCompatActivity implements View.OnClickListener{
+    private ImageButton milestoneButton;
+    private ImageButton travelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+
+        milestoneButton = (ImageButton) findViewById(R.id.milestone);
+        travelButton = (ImageButton) findViewById(R.id.travel_book);
+
+        milestoneButton.setOnClickListener(this);
+        travelButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.milestone) {
+            Intent milestoneIntent = new Intent(this, distance_log.class);
+            milestoneIntent.setAction(Intent.ACTION_VIEW);
+
+            startActivity(milestoneIntent);
+        } else if (v.getId() == R.id.travel_book) {
+            Intent travelIntent = new Intent(this, TravelBookActivity.class);
+            travelIntent.setAction(Intent.ACTION_VIEW);
+
+            startActivity(travelIntent);
+        }
     }
 }
