9e902e95fc8fa6e16ce8519c0200a3802b63d974
exsumm <xiaying2@illinois.edu>
2019-11-18 17:02:28 -0600

    create drop down, change some styling, connect home page to create run

--- RuntheWorld/app/src/main/java/com/example/run_the_world/DuringRun.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/DuringRun.java
@@ -2,13 +2,25 @@ package com.example.run_the_world;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class DuringRun extends AppCompatActivity {
+public class DuringRun extends AppCompatActivity implements View.OnClickListener{
+    private Button backHome;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_during_run);
+        backHome = (Button) findViewById(R.id.stop_run);
+        backHome.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent toHome = new Intent(this, MainActivity.class);
+        toHome.setAction(Intent.ACTION_VIEW);
+        startActivity(toHome);
     }
 }
--- RuntheWorld/app/src/main/java/com/example/run_the_world/MainActivity.java
+++ RuntheWorld/app/src/main/java/com/example/run_the_world/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button enterRunButton;
+    private Button createTripButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +17,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         enterRunButton = (Button) findViewById(R.id.enter_run);
-
+        createTripButton = findViewById(R.id.create_trip);
+        createTripButton.setOnClickListener(this);
         enterRunButton.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        Intent enterRunIntent = new Intent(this, InputRunData.class);
-        enterRunIntent.setAction(Intent.ACTION_VIEW);
-
-        startActivity(enterRunIntent);
+        if(v.getId() == R.id.enter_run){
+            Intent enterRunIntent = new Intent(this, InputRunData.class);
+            enterRunIntent.setAction(Intent.ACTION_VIEW);
+            startActivity(enterRunIntent);
+        } else {
+            Intent createTripIntent = new Intent(this,CreateNewRun.class);
+            createTripIntent.setAction(Intent.ACTION_VIEW);
+            startActivity(createTripIntent);
+        }
     }
 }
